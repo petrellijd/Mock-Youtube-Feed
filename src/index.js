0460bad3d3d7136c9883fafa1ab8cc069898cf0e
petrellijd <petrellijd@gmail.com>
2022-12-27 10:59:44 -0700

    initial search term change

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class App extends Component {
             videos: [],
             selectedVideo: null 
         };
-        this.videoSearch('surfboards')
+        this.videoSearch('deep sea fishing')
     }
 
     videoSearch(term) {
